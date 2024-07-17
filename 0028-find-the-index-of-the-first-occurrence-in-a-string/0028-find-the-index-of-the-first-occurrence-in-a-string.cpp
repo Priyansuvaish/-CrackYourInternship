8e12eff1017cad34ea274a6db70abea749b8c254
Priyanshu Vaish <106699296+Priyansuvaish@users.noreply.github.com>
2024-07-17 17:20:27 +0530

    Time: 3 ms (42.97%), Space: 7.8 MB (27.25%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,18 +3,38 @@ public:
     int strStr(string h, string n) {
         int n1=h.size();
         int n2=n.size();
-        int i=0;
-        int c=-1;
-        while(i<n1){
-            string st=h.substr(i,n2);
+        int q=INT_MAX;
+        int d=26;
+        long long hn=0;
+        long long hh=0;
+        long long p=1;
+        for(int i=0;i<n2;i++){
+            hn=(hn*d+n[i])%q;
+            hh=(hh*d+h[i])%q;
 
-            if(st==n){
-                c=i;
-                break;
+        }
+        if(hh==hn)return 0;
+        for (int l = 0; l < n2 - 1; l++)p = (p * d) % q;
+
+        int i=0;
+        int j=0;
+        while(i<=n1-n2){
+            if(hh==hn){
+                int k;
+                for(k=0;k<n2;k++){
+                    if(h[i+k]!=n[k])break;
+                }
+                if(k==n2)return i;
+            }
+            else{
+                hh=((hh-(h[i]*p))*d+h[i+n2])%q;
+                if (hh < 0)
+                hh = (hh + q);
             }
             i++;
         }
+        if(hh==hn)return i;
 
-        return c;
+        return -1;
     }
 };
