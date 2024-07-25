32c7c058aab7e98c5e3f2b8c0f23edff8527e025
Priyanshu Vaish <106699296+Priyansuvaish@users.noreply.github.com>
2024-07-25 15:40:10 +0530

    Time: 6 ms (88.64%), Space: 14.2 MB (19.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,15 +5,13 @@ public:
         vector<int> ans(2,0);
         for(int i=0;i<nums.size();i++){
             int fin= target-nums[i];
-            if(!fin) ans[0]=i;
+            if(mp.find(fin) == mp.end())mp[nums[i]]=i;
             else{
-                if(mp.find(fin) == mp.end())mp[nums[i]]=i;
-                else{
-                    ans[0]=i;
-                    ans[1]=mp[fin];
-                    break;
-                }
+                ans[0]=i;
+                ans[1]=mp[fin];
+                break;
             }
+            
         }
         return ans;
     }
